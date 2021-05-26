e3f6fc49da73ece205e88cc5ff901c6c29ef9802
Peyman27m <63470748+Peyman27m@users.noreply.github.com>
2021-05-26 17:26:54 +0430

    Update JavaQuickStart.java

--- Java/JavaQuickStart.java
+++ Java/JavaQuickStart.java
@@ -1,7 +1,7 @@
 HttpRequest request = HttpRequest.newBuilder()
 		.uri(URI.create("https://face-enroll1.p.rapidapi.com/Login"))
 		.header("content-type", "multipart/form-data; boundary=---011000010111000001101001")
-		.header("x-rapidapi-key", "f485496ab8msh2ea068a0bbe333cp179f86jsn2b4afe8962dd")
+		.header("x-rapidapi-key", "APIKEY")
 		.header("x-rapidapi-host", "face-enroll1.p.rapidapi.com")
 		.method("POST", HttpRequest.BodyPublishers.ofString("-----011000010111000001101001\r
 Content-Disposition: form-data; name=\"photo\"\r
