878ac6bb0417b8d0b58dc110d388aba45be5e88e
Peyman27m <63470748+Peyman27m@users.noreply.github.com>
2021-05-26 16:45:06 +0430

    Update JavaQuickStart.java

--- Java/JavaQuickStart.java
+++ Java/JavaQuickStart.java
@@ -1,8 +1,9 @@
-import requests
-
-url = "https://face-enroll1.p.rapidapi.com/Login"
-
-payload = "-----011000010111000001101001\r
+HttpRequest request = HttpRequest.newBuilder()
+		.uri(URI.create("https://face-enroll1.p.rapidapi.com/Login"))
+		.header("content-type", "multipart/form-data; boundary=---011000010111000001101001")
+		.header("x-rapidapi-key", "f485496ab8msh2ea068a0bbe333cp179f86jsn2b4afe8962dd")
+		.header("x-rapidapi-host", "face-enroll1.p.rapidapi.com")
+		.method("POST", HttpRequest.BodyPublishers.ofString("-----011000010111000001101001\r
 Content-Disposition: form-data; name=\"photo\"\r
 \r
 \r
@@ -12,13 +13,7 @@ Content-Disposition: form-data; name=\"personID\"\r
 <REQUIRED>\r
 -----011000010111000001101001--\r
 \r
-"
-headers = {
-    'content-type': "multipart/form-data; boundary=---011000010111000001101001",
-    'x-rapidapi-key': "f485496ab8msh2ea068a0bbe333cp179f86jsn2b4afe8962dd",
-    'x-rapidapi-host': "face-enroll1.p.rapidapi.com"
-    }
-
-response = requests.request("POST", url, data=payload, headers=headers)
-
-print(response.text)
+"))
+		.build();
+HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+System.out.println(response.body());
